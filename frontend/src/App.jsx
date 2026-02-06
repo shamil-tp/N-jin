023b2a826eb14e33232c5258d8765eec32f7cfc2
shamil <for.photos.only001@gmail.com>
2026-02-06 16:03:08 +0530

    ranking and searching completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import SearchHome from "./pages/home";
+import SearchPage from "./pages/SearchPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<SearchHome />} />
+      <Route path="/search" element={<SearchPage/>} />
       {/* Future routes */}
       {/* <Route path="/about" element={<About />} /> */}
     </Routes>
